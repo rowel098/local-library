6b94e97ef211a05d56b6d1367907f9378eea973f
Rowel Dela Fuente <rowel0987@gmail.com>
2018-11-11 20:46:06 +0800

    Add details page for author and book instance, Used lifespan virtual field for authors

--- models/author.js
+++ models/author.js
@@ -23,7 +23,7 @@ AuthorSchema
 AuthorSchema
 .virtual('lifespan')
 .get(function() {
-    return (this.date_of_death.getYear() - this.date_of_birth.getYear()).toString();
+    return this.date_of_birth_formatted + ' - ' + this.date_of_death_formatted;
 });
 
 // Virtual for author's URL
